3e5bd901c54cb0c60b5bc158b00f83b814026b18
rayhan_abdullha <captainrayhan215@gmail.com>
2026-07-09 12:05:02 +0600

    feat: implemented auth middleware and jwt utils

--- src/modules/users/routes.ts
+++ src/modules/users/routes.ts
@@ -1,11 +1,21 @@
 import { Router } from "express";
 import { userController } from "./controller";
+import { auth } from "../../middlewares/auth";
+import { Role } from "./interface";
 
 const router = Router();
 
 router.post("/register", userController.registerUser);
 
-router.patch("/profile/:id", userController.updateMyProfile);
-router.get("/me", userController.getMyProfile);
+router.patch(
+  "/profile",
+  auth(Role.ADMIN, Role.CUSTOMER, Role.PROVIDER),
+  userController.updateMyProfile,
+);
+router.get(
+  "/me",
+  auth(Role.ADMIN, Role.CUSTOMER, Role.PROVIDER),
+  userController.getMyProfile,
+);
 
 export const userRoutes = router;
