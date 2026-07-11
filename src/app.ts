d593a71c0c2676e388e46354faccf62a630073e9
rayhan_abdullha <captainrayhan215@gmail.com>
2026-07-11 14:53:46 +0600

    fix: payment routes

--- src/app.ts
+++ src/app.ts
@@ -21,6 +21,8 @@ app.use(
   }),
 );
 
+app.use("/api/v1/payments/webhook", express.raw({ type: "application/json" }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
--- src/modules/payments/payment.routes.ts
+++ src/modules/payments/payment.routes.ts
@@ -3,31 +3,15 @@ import { paymentControllers } from "./payment.controller";
 import { auth } from "../../middlewares/auth";
 import { Role } from "../users/user.interface";
 import { paymentServices } from "./payment.services";
-import express from "express";
 const router = Router();
-import { prisma } from "../../lib/prisma";
 
-router.post(
-  "/webhook",
-  express.raw({ type: "application/json" }),
-  async (req, res) => {
-    try {
-      const signature = req.headers["stripe-signature"] as string;
-      console.log("Stripe webhook signature:", signature);
+router.post("/webhook", async (req, res) => {
+  const signature = req.headers["stripe-signature"] as string;
 
-      await paymentServices.confirmPaymentWebhook(signature, req.body);
-      console.log("Stripe webhook processed successfully");
-      res.status(200).json({
-        received: true,
-      });
-    } catch (error: any) {
-      console.log("Error processing Stripe webhook:", error.message);
-      res.status(400).json({
-        message: error.message,
-      });
-    }
-  },
-);
+  await paymentServices.confirmPaymentWebhook(signature, req.body);
+
+  res.sendStatus(200);
+});
 
 router.post(
   "/create",
@@ -36,28 +20,13 @@ router.post(
 );
 router.get(
   "/",
-  auth(Role.ADMIN, Role.PROVIDER, Role.CUSTOMER),
+  auth(Role.ADMIN, Role.PROVIDER),
   paymentControllers.getPaymentHistory,
 );
 router.get(
   "/:id",
-  auth(Role.ADMIN, Role.PROVIDER, Role.CUSTOMER),
+  auth(Role.ADMIN, Role.PROVIDER),
   paymentControllers.getPaymentDetails,
 );
-
-router.delete("/", async function async(req, res) {
-  try {
-    await prisma.payment.deleteMany({});
-    // await prisma.orderItem.deleteMany({});
-    // await prisma.order.deleteMany({});
-
-    res.status(200).json({ message: "All payments and orders deleted" });
-  } catch (error: any) {
-    res.status(500).json({
-      message: "Error deleting payments and orders",
-      error: error.message,
-    });
-  }
-});
 
 export default router;
