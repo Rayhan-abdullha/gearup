300f58675ca30607de85f5dc687af548807536c0
rayhan_abdullha <captainrayhan215@gmail.com>
2026-07-15 10:55:54 +0600

    feat: added review  zod schema

--- src/modules/review/review.routes.ts
+++ src/modules/review/review.routes.ts
@@ -2,9 +2,16 @@ import { Router } from "express";
 import { Role } from "../users/user.interface";
 import { auth } from "../../middlewares/auth";
 import { reviewController } from "./review.controller";
+import validateRequest from "../../middlewares/zodValidationRequest";
+import { createReviewSchema } from "./review.schema";
 
 const router = Router();
 
-router.post("/", auth(Role.CUSTOMER), reviewController.createReview);
+router.post(
+  "/",
+  auth(Role.CUSTOMER),
+  validateRequest(createReviewSchema),
+  reviewController.createReview,
+);
 
 export const reviewRoutes = router;
---
+++ src/modules/review/review.schema.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+export const createReviewSchema = z.object({
+  body: z.object({
+    gearId: z.string().uuid("Invalid gear reference UUID"),
+    rating: z
+      .number()
+      .int()
+      .min(1, "Minimum rating is 1 star")
+      .max(5, "Maximum rating is 5 stars"),
+    comment: z
+      .string()
+      .max(500, "Comment cannot exceed 500 characters")
+      .optional(),
+  }),
+});
