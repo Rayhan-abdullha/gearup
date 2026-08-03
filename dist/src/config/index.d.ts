8e9e9f8705697a2e82e4ae50ffe1a0ccca208819
rayhan_abdullha <captainrayhan215@gmail.com>
2026-08-03 16:02:54 +0600

    fix: cors issues

--- dist/src/config/index.d.ts
+++ dist/src/config/index.d.ts
@@ -1,7 +1,7 @@
 declare const _default: {
     port: string | undefined;
     database_url: string | undefined;
-    app_url: string | undefined;
+    app_url: string;
     bcrypt_salt_rounds: string | undefined;
     jwt_access_secret: string;
     jwt_refresh_secret: string;
